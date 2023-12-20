df94373ed091de30393cab18859e27fee3e79ef3
Katiadje <145567771+Katiadje@users.noreply.github.com>
2023-12-20 11:24:24 +0100

    Update UserController.java

--- backEnd/spacelearning/src/main/java/dev/spacelearning/spacelearning/UserController.java
+++ backEnd/spacelearning/src/main/java/dev/spacelearning/spacelearning/UserController.java
@@ -5,27 +5,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
 
     private final List<Eleve> eleveList = List.of(
-            new Eleve("Dupont", "Jean", "jeand", 21, "ESIEE Paris"),
-            new Eleve("Martin", "Marie", "mariem", 22, "Sorbonne Université"),
-            new Eleve("Leblanc", "Pierre", "pierrel", 23, "Université Paris-Sud"),
-            new Eleve("Lefevre", "Warren", "warrenl", 24, "Université Paris Diderot"),
-            new Eleve("Simon", "Claire", "claires", 24, "Télécom Paris"),
-            new Eleve("Girard", "Luc", "lucg", 19, "Université Paris Dauphine"),
-            new Eleve("Lefort", "Sophie", "sophiel", 22, "HEC Paris"),
-            new Eleve("Mbappe", "Alexandre", "alexm", 22, "Université Paris Descartes"),
-            new Eleve("Messi", "Lionel", "leom", 23, "ENS Paris"),
-            new Eleve("Ronaldo", "Chris", "chrisr", 23, "Université Paris Nanterre")
+            new Eleve("jean.d@example.com", "password1"),
+            new Eleve("marie.m@example.com", "password2"),
+            new Eleve("pierre.l@example.com", "password3"),
+            new Eleve("warren.l@example.com", "password4"),
+            new Eleve("claire.s@example.com", "password5"),
+            new Eleve("luc.g@example.com", "password6"),
+            new Eleve("sophie.l@example.com", "password7"),
+            new Eleve("alexandre.m@example.com", "password8"),
+            new Eleve("lionel.m@example.com", "password9"),
+            new Eleve("chris.r@example.com", "password10")
     );
 
     @GetMapping
-    public List<Eleve> getUsers() {
-        return eleveList;
+    public List<UserDTO> getUsers() {
+        // Return a list of users with only email and password
+        return eleveList.stream()
+                .map(user -> new UserDTO(user.getEmail(), user.getMotDePasse()))
+                .collect(Collectors.toList());
+    }
+
+    // Define a DTO class for User with only email and password
+    private static class UserDTO {
+        private final String email;
+        private final String motDePasse;
+
+        public UserDTO(String email, String motDePasse) {
+            this.email = email;
+            this.motDePasse = motDePasse;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getMotDePasse() {
+            return motDePasse;
+        }
     }
 }
 
